b58e610a8fdac8ff980fe6f7332b97b9b2e02ee1
Rr2410 <123rr456aa@gmail.com>
2024-05-01 15:51:35 +0300

    added functionlity to search bar, data now dynamically fetched from database

--- app.js
+++ app.js
@@ -40,7 +40,97 @@ app.post('/login', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+//display patient data
+app.get('/data', async (req, res) => {
+  const { offset, limit } = req.query;
+
+  console.log('Offset:', offset);
+  console.log('Limit:', limit);
+
+  try {
+    const [results] = await db.execute(`SELECT PatientID, FirstName, LastName, Gender, MedicalHistory, ImageProcessingHistory, responsibleEmpId FROM Patients LIMIT ${parseInt(offset)}, ${parseInt(limit)}`);
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//searching in patient table
+app.get('/data', async (req, res) => {
+  const { offset, limit, search } = req.query;
+
+  console.log('Offset:', offset);
+  console.log('Limit:', limit);
+  console.log('Search:', search);
+
+  let query = 'SELECT PatientID, FirstName, LastName, Gender, MedicalHistory, ImageProcessingHistory, responsibleEmpId FROM Patients';
+  let params = [];
+
+  if (search) {
+    query += ' WHERE PatientID LIKE ? OR MedicalHistory LIKE ? OR ImageProcessingHistory LIKE ? OR  FirstName LIKE ? OR LastName LIKE ? OR Gender LIKE ?  OR responsibleEmpId LIKE ?';
+    params = [`%${search}%`, `%${search}%`, `%${search}%`];
+  }
+
+  query += ` LIMIT ${parseInt(offset)}, ${parseInt(limit)}`;
+
+  try {
+    const [results] = await db.execute(query, params);
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+//dispaly employees data
+app.get('/data-emp', async (req, res) => {
+  const { offset, limit } = req.query;
+
+  console.log('Offset:', offset);
+  console.log('Limit:', limit);
+
+  try {
+    const [results] = await db.execute(`SELECT EmpID, FirstName, LastName, Job, Administration, Access, supervisorId FROM Employee LIMIT ${parseInt(offset)}, ${parseInt(limit)}`);
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//search the employee table
+app.get('/emp-data', async (req, res) => {
+  const { offset, limit, search } = req.query;
+
+  console.log('Offset:', offset);
+  console.log('Limit:', limit);
+  console.log('Search:', search);
+
+  let query = 'SELECT EmpID, FirstName, LastName, Job, Administration, Access, supervisorId FROM Employee';
+  let params = [];
+
+  if (search) {
+    query += ' WHERE EmpID LIKE ? OR FirstName LIKE ? OR LastName LIKE ? OR  Job LIKE ? OR Administration LIKE ? OR Access LIKE ?  OR supervisorId LIKE ?';
+    params = [`%${search}%`, `%${search}%`, `%${search}%`];
+  }
+
+  query += ` LIMIT ${parseInt(offset)}, ${parseInt(limit)}`;
+
+  try {
+    const [results] = await db.execute(query, params);
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
