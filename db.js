7ac17e2c1b8ae9622683a47d33c88210d4379650
Rr2410 <123rr456aa@gmail.com>
2024-05-07 22:27:16 +0300

    connectiond details changed

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '@G123',
-  database: 'GP2',
+  host: 'mysql-22ed2874-rr456aa-117e.f.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS_S2185k_gM0yQwl1y07K',
+  database: 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
